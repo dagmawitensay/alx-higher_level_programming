c00d300a7a70290d51f57ee940866f27185bf108
dagiTensay <dagitensay@gmai.com>
2023-03-07 12:43:26 -0800

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i == 100:
+            print("Buzz")
+        else:
+            if i % 3 == 0 and i % 5 == 0:
+                print("FizzBuzz ", end="")
+            elif i % 5:
+                print("Buzz ", end="")
+            elif i % 3:
+                print("Fizz ", end="")
+            else:
+                print(f"{i} ", end="")
