057c61061e6422fedc17c856079c1adb2ab400ac
dagiTensay <dagitensay@gmai.com>
2023-03-09 02:08:34 -0800

    commnad line argumnets

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif len(argv) == 2:
         print("1 argument:")
-        print("{}".format(argv[1]))
+        print("{}: {}".format(1, argv[1]))
     else:
         print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
