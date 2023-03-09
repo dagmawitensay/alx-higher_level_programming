e7996069269427a148e306d0fe6d39e8e3217896
dagiTensay <dagitensay@gmai.com>
2023-03-09 02:59:27 -0800

    compiled file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import hidden_4
 
-names = dir(hidden_4)
-for i in names:
-    if i.startswith("__"):
-        continue
-    else:
-        print("{}".format(i))
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    for i in names:
+        if i.startswith("__"):
+            continue
+        else:
+            print("{}".format(i))
