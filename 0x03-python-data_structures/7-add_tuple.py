2205c4bbe6333a301c4ad44185569e6078043f6d
dagiTensay <dagitensay@gmai.com>
2023-03-11 11:33:59 -0800

    tuple add

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,5 @@
 #!/urs/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    temp = []
     if not tuple_a:
         return tuple_b
     elif not tuple_b:
@@ -10,6 +9,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a1 = tuple_a[1] if len(tuple_a) >= 2 else 0
         b0 = tuple_b[0]
         b1 = tuple_b[1] if len(tuple_b) >= 2 else 0
-        temp.append(a0 + b0)
-        temp.append(a1 + b1)
-    return tuple(temp)
+    return (a0 + b0, a1 + b1)
