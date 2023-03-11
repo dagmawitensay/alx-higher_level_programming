34755fa24826d3c45550487c4442893814365d7b
dagiTensay <dagitensay@gmai.com>
2023-03-11 09:15:34 -0800

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        print()
+        return None
     for item in matrix:
         for value in item:
-            print("{:d}".format(value), end=" "
-                    if value != item[-1] else '\n')
+            print("{:d}".format(
+                value), end=" " if value != item[-1] else '\n')
