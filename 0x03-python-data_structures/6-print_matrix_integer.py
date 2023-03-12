c6acd73fa57adcee959f4b34b256078f8a415c25
dagiTensay <dagitensay@gmai.com>
2023-03-12 00:40:54 -0800

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        return None
+        return []
     for item in matrix:
         for value in item:
             print("{:d}".format(
