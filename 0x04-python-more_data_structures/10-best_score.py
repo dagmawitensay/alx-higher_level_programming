2f391de50922d8cc8947c6332acc8d27eb6a8518
dagiTensay <dagitensay@gmai.com>
2023-03-16 06:36:18 -0700

    max score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     max_score = 0
     scorer = ""
