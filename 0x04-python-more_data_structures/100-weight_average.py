1c03eb2cb3d0a299cbd902f9ff3993a053bf43e8
dagiTensay <dagitensay@gmai.com>
2023-03-16 06:54:09 -0700

    average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    total_sum = 0
+    div = 0
+    for item in my_list:
+        total_sum += item[0] * item[1]
+        div += item[1]
+    return total_sum / div
