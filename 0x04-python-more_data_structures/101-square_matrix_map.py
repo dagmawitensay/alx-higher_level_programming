d631e49a9a1fa025d965c0b33f296203505c81b6
dagiTensay <dagitensay@gmai.com>
2023-03-17 11:37:04 -0700

    map squared

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    squared = list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
-    return squared
+    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
