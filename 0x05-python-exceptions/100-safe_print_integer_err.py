24c1b41d937088d6da8501110f6fd03f0c64ddaa
dagiTensay <dagitensay@gmai.com>
2023-03-27 02:52:14 -0700

    print to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception as e:
-        print("Exception: ", e, file=sys.stderr)
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
     
     return True
