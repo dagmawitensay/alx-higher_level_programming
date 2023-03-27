4a79eecaef93fe984d0c364222e1712cd6c4777e
dagiTensay <dagitensay@gmai.com>
2023-03-27 02:59:12 -0700

    function exception

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(args[0], args[1])
+    except Exception as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return
