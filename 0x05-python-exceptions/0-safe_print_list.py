5c7cb44a56eebc6cf93d4e8ab8c40bddb31ec6ec
dagiTensay <dagitensay@gmai.com>
2023-03-27 02:04:40 -0700

    add error type

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             ctr += 1
-    except:
+    except IndexError:
         print()
         return ctr
     else:
