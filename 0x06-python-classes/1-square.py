911d6a00d807a299493e249353167301c04c40ee
dagiTensay <dagitensay@gmai.com>
2023-03-28 11:46:44 -0700

    add sizes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines a square class
+"""
+
+
+class Square:
+    """Class that defines a square
+    Args:
+        size : size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
