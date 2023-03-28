9d1492b33553a409aa115e3b0f25f62b05fe379a
dagiTensay <dagitensay@gmai.com>
2023-03-28 12:02:19 -0700

    compute area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module defines a square with given size
+"""
+
+
+class Square:
+    """
+    A square class that defines a sqaare instace with
+    a given size
+    Args:
+        size (int): size of the square
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        returns the area of the square
+        """
+        return self.__size ** 2
