b67ddca6de39ac6d27e33dff875c10f473c097e5
dagiTensay <dagitensay@gmai.com>
2023-04-03 10:53:44 -0700

    define width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ This module defines a Rectangle
 class Rectangle:
     """Rectangle class that defines a rectangle"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
