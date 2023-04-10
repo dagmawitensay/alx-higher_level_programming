9e89aa17a09fc17cdb65cc77752bc213952da24f
dagiTensay <dagitensay@gmai.com>
2023-04-10 03:51:32 -0700

    add readme

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines a simple method
+to add to given numbers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Returns a + b
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError(" b must be an integer")
+
+    return int(a) + int(b)
