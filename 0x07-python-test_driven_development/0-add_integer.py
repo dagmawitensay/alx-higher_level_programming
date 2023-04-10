73dc18c2b2159612f8ea7d3be89cab5767ec6486
dagiTensay <dagitensay@gmai.com>
2023-04-10 04:04:42 -0700

    fix casting

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,9 +13,9 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError(" b must be an integer")
-    if type(a) is not int:
+    if type(a) is float:
         a = int(a)
-    if type(b) is not int:
+    if type(b) is float:
         b = int(b)
 
     return a + b
