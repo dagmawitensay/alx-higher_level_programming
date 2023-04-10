7ea6e70c8bf21a9e61ba6856100c4b8420e30c35
dagiTensay <dagitensay@gmai.com>
2023-04-10 04:46:38 -0700

    add testcases

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""
+This module defines a function  matrix_divided
+"""
+
+
 def matrix_divided(matrix, div):
+    """
+    Accepts a matrix and returns a new matrix with elements divided
+    by given value div
+    Args:
+        matrix: input matrix
+        div: value to divide elements of the matrix
+
+    Return:
+        new matrix with elemnts of input matrix devided by div
+    """
     if matrix == [[]]:
         return matrix
     row_len = len(matrix[0])
@@ -14,7 +29,8 @@ def matrix_divided(matrix, div):
         temp = []
         for i in row:
             if type(i) not in [int, float]:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError
+            ("matrix must be a matrix (list of lists) of integers/floats")
             temp.append(round(i / div, 2))
         result.append(temp)
 
