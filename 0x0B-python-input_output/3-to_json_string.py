479d18d47c5e64d1d57fbda43c6cd85967318513
dagiTensay <dagitensay@gmai.com>
2023-04-11 14:49:19 -0700

    to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module contains a function that returns the json repretaion of object
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """
+    Returns the json representation of the object
+    """
+    return json.dumps(my_obj)
