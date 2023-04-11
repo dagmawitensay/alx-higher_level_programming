fc934408ebb91ff8d728d8c791d760b3f4225c89
dagiTensay <dagitensay@gmai.com>
2023-04-11 14:25:21 -0700

    fix new line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,10 +8,9 @@ opens a file.
 def read_file(filename=""):
     """
     Opens a file and prints the contents of the file.
+    Args:
+        filename: name of the file to be opened
     """
-    if filename == "":
-        print()
-    else:
-        with open(filename, 'r') as file:
-            data = file.read()
-            print(data)
+    with open(filename, 'r', encoding="UTF-8") as file:
+        data = file.read()
+        print(data, end='')
