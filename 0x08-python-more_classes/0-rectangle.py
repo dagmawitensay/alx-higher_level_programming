812765de2a95038038c5745911f95bac3f07517f
dagiTensay <dagitensay@gmai.com>
2023-05-02 15:11:10 -0700

    update folders

--- 0x08-python-more_classes/0-rectangle.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-This module defines a Rectangle
-"""
-
-
-class Rectangle:
-    """Rectangle class that defines a rectangle"""
-    pass
--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string(my_list=[]):
-    my_list.append("BestSchool")
-    return ', '.join(my_list)
