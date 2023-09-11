bc4b10b8fa3d43da8c8c98774d1ef35183951126
dagiTensay <dagitensay@gmail.com>
2023-09-11 02:19:07 -0700

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -11,4 +11,6 @@ class Rectangle(BaseGeometry):
     """
     def __init__(self, width, height):
         super().integer_validator("width", width)
+        self.__width = width
         super().integer_validator("height", height)
+        self.__height = height
