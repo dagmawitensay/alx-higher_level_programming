5fa5093acfc5cd3353c50e8049f9ffbcc9e0a8c8
dagiTensay <dagitensay@gmail.com>
2023-09-11 02:07:40 -0700

    fix task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,4 +5,7 @@ This module provides a function called `inherits_from`
 
 
 def inherits_from(obj, a_class):
+    """
+    Checks if obj is a subclass of a_class
+    """
     return issubclass(obj, a_class)
