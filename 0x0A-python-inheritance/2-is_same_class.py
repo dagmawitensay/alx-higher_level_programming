1db00ab95b4a0933721cc706e3d5e8fa6bee1d29
dagiTensay <dagitensay@gmail.com>
2023-09-11 01:27:49 -0700

    fix class type, add task3

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,7 +6,7 @@ This module provides a function `is_same_class`
 
 
 def is_same_class(obj, a_class):
-    if isinstance(obj, a_class):
+    if issubclass(obj, a_class):
         return True
 
     return False
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+This module provides a function called `is_kind_of_class`.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    return isinstance(obj, a_class)
