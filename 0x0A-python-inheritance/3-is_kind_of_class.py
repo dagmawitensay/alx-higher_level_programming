731544838c22048d96e94951ad16ab4f00b23f3c
dagiTensay <dagitensay@gmail.com>
2023-09-11 02:06:46 -0700

    fix task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,4 +5,7 @@ This module provides a function called `is_kind_of_class`.
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    Check is obj is an instance of a_class
+    """
     return isinstance(obj, a_class)
