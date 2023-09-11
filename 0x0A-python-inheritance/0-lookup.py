b944c8eb3eeb033fae88e88f9dffe5fff7dff263
dagiTensay <dagitensay@gmail.com>
2023-09-11 00:46:55 -0700

    add Readme

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module provides one function called "lookup"
+"""
+
+
+
+def lookup(obj):
+    """Prints available methods and attributes of the given object"""
+    return dir(obj)
