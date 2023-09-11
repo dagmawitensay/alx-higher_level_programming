dc95e9e766e402441fd2a4db12d145e5b8e003f9
dagiTensay <dagitensay@gmail.com>
2023-09-11 01:39:52 -0700

    add tas6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module provides a calss called `BaseGeometry`
+"""
+
+
+class BaseGeometry:
+    """
+    A base geometry class
+    Public_Methods:
+        area(self): raise an exception:
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
