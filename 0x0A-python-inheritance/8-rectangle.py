3bd5f7b32ab50b8001f33026b9ef5417d8887f68
dagiTensay <dagitensay@gmail.com>
2023-09-11 02:01:27 -0700

    add task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+This module provides a class called `Rectangle`
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    This class inherits from Base Geometry class.
+    """
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
