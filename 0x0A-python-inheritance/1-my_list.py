f45114c9babc34dd250dd44140381021a0555082
dagiTensay <dagitensay@gmail.com>
2023-09-11 01:18:12 -0700

    add task1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This class provides a class called MyList"""
+
+class MyList(list):
+    """
+    MyList is a subclass of list that provides additional functionality.
+
+    Public Methods:
+        print_sorted(): Print the list in ascending order.
+    """
+    def print_sorted(self):
+        sorted_list = sorted(self)
+        print(sorted_list)
