54b49e9262ab53792844903825b200a79c276acf
dagiTensay <dagitensay@gmail.com>
2023-09-13 01:50:55 -0700

    add task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module provies `student` class.
+"""
+
+
+class Student:
+    """
+    A student class with student assciated attributes
+    and a method to return dictinary representation of student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """Instantiation of student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns Json representation of the object.
+        """
+        ans = self.__dict__
+        if attrs:
+            res = {item: ans[item] for item in attrs if item in ans}
+            return res
+
+        return ans
