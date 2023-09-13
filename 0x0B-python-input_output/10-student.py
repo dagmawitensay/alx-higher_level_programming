95e51d504a89352f02e176e3013e02d4cba70ca2
dagiTensay <dagitensay@gmail.com>
2023-09-13 02:00:36 -0700

    fix task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
-"""
-This module provies `student` class.
+""" Module that defines the class Student
 """
 
 
 class Student:
-    """
-    A student class with student assciated attributes
-    and a method to return dictinary representation of student.
-    """
+    """ Class to create student instances """
+
     def __init__(self, first_name, last_name, age):
-        """Instantiation of student"""
+        """ Special method to initialize """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Returns Json representation of the object.
-        """
-        ans = self.__dict__
-        if attrs and type(attrs) is list:
+        """ Method that returns directory description """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+
             for item in attrs:
-                if type(item) != str:
-                    return ans
+                if type(item) is not str:
+                    return obj
 
-            res = {item: ans[item] for item in attrs if item in ans}
-            return res
+            d_list = {}
 
-        return ans
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+
+        return obj
