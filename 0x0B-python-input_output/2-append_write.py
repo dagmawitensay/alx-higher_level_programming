c9e0eb42ef0117beff11727bd67c1e5cd9754927
dagiTensay <dagitensay@gmail.com>
2023-09-13 00:04:44 -0700

    add task 1

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module provide  a function `append_write`.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Append the given text to the end of the file with
+    the given filename.
+    Args:
+        filename: name of the file.
+        text: content to be written.
+    """
+    with open(filename, 'a') as file:
+        file.write(text)
+    
+    return len(text)
