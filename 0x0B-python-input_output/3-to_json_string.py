5871662aa4c40f4fb44ccbb236476f1a62fcf2db
dagiTensay <dagitensay@gmail.com>
2023-09-13 00:15:31 -0700

    fix task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import json
 """
 This module provides a functin `to_json_string`
 """
+import json
 
 
 def to_json_string(my_obj):
     """
     Returns the JSON representation of a given object.
+    Args:
+        my_obj: object to be converted.
     """
     return json.dumps(my_obj)
