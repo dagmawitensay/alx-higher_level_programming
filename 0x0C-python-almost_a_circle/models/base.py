00d2d5f83e382699cbea46cda8e931f00e3389cb
dagiTensay <dagitensay@gmail.com>
2023-09-15 07:46:59 -0700

    add task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,10 +18,34 @@ class Base:
             id = Base.__nb_objects
         self.id = id
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         Returns the JSON sring reprsentation of list_dictionaries.
+        Args:
+            list_dictionaries: list of dictionaries
         """
         if not list_dictionaries or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation of list_objs to a file.
+        Args:
+            list_objs: list of objects
+        """
+        name = cls.__name__ + ".json"
+        with open(name, 'w') as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                dictionary = [obj.to_dictionary() for obj in list_objs]
+                file.write(Base.to_json_string(dictionary))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representation json_string.
+        """
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 
@@ -12,3 +13,9 @@ class TestBaseClass(unittest.TestCase):
     def test_Empty_instance(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
+
+    def test_dict_to_json(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(type(json_dictionary), str)
