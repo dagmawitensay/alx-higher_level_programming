fa29356f52a98c610719a464a84f90fb69038b24
dagiTensay <dagitensay@gmail.com>
2023-09-22 02:20:35 -0700

    add task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
 class LockedClass:
     __slots__ = ('firt_name',)
+
     def __setatr__(self, name, value):
         if name != 'first_name':
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
+            raise AttributeError(
+                    "'LockedClass' object has no attribute '{}'".format(name))
         self.__dict__[name] = value
